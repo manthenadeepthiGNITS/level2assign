8fca5e5f5ccac1b1f85f6e97a6f596a627d630e6
manthenadeepthiGNITS <manthena.deepthi@gnits.ac.in>
2021-09-17 15:18:06 +0530

    including -filtering records of zone file

--- lookup.rb
+++ lookup.rb
@@ -17,55 +17,35 @@ domain = get_command_line_argument
 # array of string, where each element is a line
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
-#puts (dns_raw)
-
 
 def parse_dns(rec)
+  noEmptyRecords = rec.reject { |c| c=="\n" || c[0]=='#'}
   hsh = {}
-  rec.each{ |precord|
+  noEmptyRecords.each{ |precord|
     k=precord.split()
     k[0]=k[0].chop
     k[1]=k[1].chop
-    #puts(k[0])
-    #puts(k[1])
     hsh[k[1]] = {type:k[0],target:k[2]}}
     #puts hsh
   return(hsh)
 end
 
-def resolve(d_records,lookup_chain,domain)
-  if d_records[domain]!=nil
-     ans=d_records[domain][:type]
-     #puts ans+"\n"
-      if ans=="CNAME"
-        lookup_chain.push(d_records[domain][:target])
-        #puts "***hi1"
-        resolve(d_records,lookup_chain,d_records[domain][:target])
-      else
-        #puts "hi2777"
-        lookup_chain.push(d_records[domain][:target])
-        return lookup_chain
-      end
-  else
-    if lookup_chain.length==1
-      dom_name=lookup_chain[0]
-      lookup_chain[0]="error :record not found for #{dom_name}"
-      #lookup_chain.push(" ")
-    else
-      return
-    end
-
+def resolve(dns_records,lookup_chain,domain)
+  record = dns_records[domain]
+  if (!record)
+    domain=lookup_chain[0]
+    lookup_chain[0]="Error: Record not found for "
+    lookup_chain[1]=domain
+    return lookup_chain
+  elsif record[:type] == "CNAME"
+    lookup_chain.push(dns_records[domain][:target])
+    resolve(dns_records,lookup_chain,dns_records[domain][:target])
+  elsif record[:type] == "A"
+    lookup_chain.push(dns_records[domain][:target])
   end
-  #puts lookup_chain
-  return(lookup_chain)
 end
 
-
-# To complete the assignment, implement `parse_dns` and `resolve`.
-# Remember to implement them above this line since in Ruby
-# you can invoke a function only after it is defined.
 dns_records = parse_dns(dns_raw)
-#puts dns_records
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
 puts lookup_chain.join(" => ")
